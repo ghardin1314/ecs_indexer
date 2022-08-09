071c0dee4bb96c785844bf62069377249534d77a
ghardin1314 <g.hardin1314@gmail.com>
2022-08-09 14:43:27 -0700

    read from manifest yaml

--- src/components.rs
+++ src/components.rs
@@ -1,6 +1,6 @@
 use crate::prelude::*;
 
-use ethers::abi::Event;
+use ethers::{abi::Event, types::Address};
 
 #[derive(Component)]
 struct Contract {}
@@ -8,7 +8,11 @@ struct Contract {}
 #[derive(Component)]
 pub struct EventTrigger {
     pub event: Event,
+    pub address: Option<Address>,
 }
 
 #[derive(Component)]
 struct TriggerAction {}
+
+#[derive(Component)]
+struct StartBlock(usize);
--- src/systems/load_config.rs
+++ src/systems/load_config.rs
@@ -5,24 +5,45 @@ use ethers::{
     prelude::*,
     utils::Anvil,
 };
-use std::fs::File;
+use std::{fs::File, io::Read};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct Manifest {
-    contracts: Vac<ContractData>,
+    contracts: Vec<ContractData>,
 }
 
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct ContractData {
     abi: String,
 }
 
 pub fn load_config(mut commands: Commands) {
-    let file = File::open("./resources/Gravity.json").unwrap();
-    let abi = Abi::load(file).unwrap();
+    let manifest = open_manifest("./resources/manifest.yaml".to_owned()).unwrap();
 
-    for event in abi.events() {
-        commands.spawn().insert(EventTrigger {
-            event: event.clone(),
-        });
+    for contract_data in manifest.contracts.iter() {
+        let file = File::open(contract_data.abi.clone()).unwrap();
+        let abi = Abi::load(file).unwrap();
+
+        for event in abi.events() {
+            commands.spawn().insert(EventTrigger {
+                event: event.clone(),
+                address: None,
+            });
+        }
+    }
+}
+
+fn open_manifest(path: String) -> Option<Manifest> {
+    match File::open(path) {
+        Ok(mut file) => {
+            let mut content = String::new();
+            file.read_to_string(&mut content).unwrap();
+
+            let manifest: Manifest = serde_yaml::from_str(&content).unwrap();
+            Some(manifest)
+        }
+        Err(error) => {
+            panic!("No manifest file found")
+        }
     }
 }
